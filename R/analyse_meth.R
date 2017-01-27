969f728ce3675d99542dba051da1489fed68d2aa
Florent Chuffart <florent.chuffart@gmail.com>
2017-01-27 17:16:53 +0100

    Adding analyse_trscr_cnv.Rd and analyse_cnv.Rd features

--- R/analyse_meth.R
+++ R/analyse_meth.R
@@ -1,11 +1,165 @@
+#' A Function Analyses CNV data
+#'
+#' This function analyses methylome data.
+#' @param gene A vector describing the gene (line of a bed file).
+#' @param cnv_data A matrix of CN values.
+#' @param cnv_platform A data frame describing probe positions.
+#' @param cols A color vectors indexed by by samples names.
+#' @param PLOT A boolean defining if graphical output must be dispayed on the graphical output.
+#' @param up_str   An integer specifying up stream size (in bp).
+#' @param dwn_str  An integer specifying down stream size (in bp).
+#' @importFrom graphics legend
+#' @importFrom graphics axis
+#' @export
+analyse_cnv = function(gene, cnv_data, cnv_platform, cols, PLOT=FALSE, up_str=5000, dwn_str=5000) {
+  # params
+  sample_names = colnames(cnv_data)
+  if (missing(cols)) {
+    cols=c()
+    cols[sample_names] = 1
+  }
+
+  # get gene properties
+  chr =              gene[[1]]
+  beg =   as.numeric(gene[[2]])
+  end =   as.numeric(gene[[3]])
+  gene_name =        gene[[4]]
+  id_probe =         gene[[5]]
+  strand =           gene[[6]]
+
+  # get cnv infos
+  if (strand == "-") {
+    off_set_beg = dwn_str
+    off_set_end = up_str
+  } else {
+    off_set_beg = up_str
+    off_set_end = dwn_str
+  }  
+  probe_before = rev(rownames(cnv_platform)[!is.na(cnv_platform$chrom)  & cnv_platform$chrom == chr & cnv_platform$loc < beg-off_set_beg])[1]
+  probe_after =      rownames(cnv_platform)[!is.na(cnv_platform$chrom)  & cnv_platform$chrom == chr & cnv_platform$loc > end+off_set_end] [1]
+  if (is.na(probe_after)) {
+    probe_after =    rev(rownames(cnv_platform)[!is.na(cnv_platform$chrom)  & cnv_platform$chrom == chr]) [1]
+  }
+  idx = which(rownames(cnv_platform) == probe_before):which(rownames(cnv_platform) == probe_after)
+  probes_cnv = rownames(cnv_platform)[idx]
+  pos_cnv = cnv_platform[probes_cnv, ]$loc
+  probes_cnv_close = rownames(cnv_platform)[idx]
+
+  cnv_val = apply(cnv_data[probes_cnv_close, sample_names],2, function(c) {
+    if (length(unique(c)) == 1) {
+      return(unique(c)[1])
+    } else {
+      return(NA)
+    }
+  })
+
+  if (PLOT) { 
+    max_cnv = 8
+    y_base = 0
+    plot(0, 0, col=0, xaxt="n",
+      xlim=range(c(pos_cnv)), ylim=c(0, max_cnv)                          , 
+      ylab="CN",                                                          , 
+      xlab=paste("chr", gene[[1]], ":", gene[[2]], "-", gene[[3]], sep=""),
+      main=paste(gene_name, sep="")
+    )
+    axis(1, at=gene[2:3])
+    polygon(c(beg,end,end,beg), c(y_base-0.1, y_base-0.1, y_base+0.1, y_base+0.1), col=1)
+    if (strand == "-") {
+      arrows(end, y_base, end, y_base+0.3, length=0)
+      arrows(end, y_base+0.3, end - (end-beg)/3, y_base+0.3, length=0.03)
+    } else {
+      arrows(beg, y_base, beg, y_base+0.3, length=0)
+      arrows(beg, y_base+0.3, beg + (end-beg)/3, y_base+0.3, length=0.03)
+    }  
+    points(pos_cnv , rep(y_base      , length(pos_cnv )), pch=16, col=adjustcolor(2, alpha.f=0.6))
+    matplot(pos_cnv, cnv_data[probes_cnv,sample_names], type="l", lty=1, col=adjustcolor(cols[sample_names], alpha.f=0.5), lwd=3, add=TRUE)
+    
+  }
+  return(cnv_val)
+}
+
+
+#' A Function Analyses CNV and Transcriptome data
+#'
+#' This function analyses methylome data.
+#' @param gene A vector describing the gene (line of a bed file).
+#' @param trscr_res A vector of expression values.
+#' @param cnv_res A vector of CN values.
+#' @param meth_idx A vector of sample for which methylom analysis is available.
+#' @param cols A color vectors indexed by by samples names.
+#' @param idxes A list of index defining sample groups. 
+#' @param ctrl_idx A vector of sample used to define threshold 
+#' @param PLOT A boolean defining if graphical output must be dispayed on the graphical output.
+#' @importFrom graphics legend
+#' @export
+analyse_trscr_cnv = function(gene, trscr_res, cnv_res, meth_idx, ctrl_idx, cols, idxes, PLOT=TRUE) {
+  # get gene properties
+  chr =              gene[[1]]
+  beg =   as.numeric(gene[[2]])
+  end =   as.numeric(gene[[3]])
+  gene_name =        gene[[4]]
+  id_probe =         gene[[5]]
+  strand =           gene[[6]]
+  
+  trscr_res_orig = trscr_res
+  trscr_res = trscr_res[names(cnv_res)]
+
+  # thresholds
+  if (missing(cols) | missing(idxes)) {
+    trscr_thres_hi = trscr_thres_lo = max(trscr_res_orig[ctrl_idx])
+    cnv_thres_hi = 3.5
+    cnv_thres_lo = 2.5
+    trscr_lo_cnv_lo_idx = names(trscr_res)[!is.na(cnv_res) & trscr_res < trscr_thres_lo & cnv_res < cnv_thres_lo]
+    trscr_hi_cnv_lo_idx = names(trscr_res)[!is.na(cnv_res) & trscr_res >= trscr_thres_hi & cnv_res < cnv_thres_lo]
+    trscr_hi_cnv_hi_idx = names(trscr_res)[!is.na(cnv_res) & trscr_res > trscr_thres_hi & cnv_res >= cnv_thres_hi]
+    idxes = list(trscr_hi_cnv_hi=trscr_hi_cnv_hi_idx,
+                 trscr_hi_cnv_lo=trscr_hi_cnv_lo_idx,
+                 trscr_lo_cnv_lo=trscr_lo_cnv_lo_idx)
+  } else {
+    idxes = idxes
+  }
+  
+  # cols
+  sample_names = names(cnv_res)
+  if (missing(cols)) {
+    cols = rep(NA, length(cnv_res))
+    names(cols) = names(cnv_res)
+    cols[!is.na(cnv_res)] = "grey"  
+    cols[idxes$trscr_hi_cnv_hi] = "red"
+    cols[idxes$trscr_hi_cnv_lo] = "green"
+    cols[idxes$trscr_lo_cnv_lo] = "blue"
+  } else {
+    cols = cols[sample_names]
+  }
+
+  # cols
+
+  idx = names(cnv_res)
+  idx2 = meth_idx
+  idx2 = idx2[idx2 %in% idx]
+  cex = pch = c()
+  pch[idx] = cex[idx] = 1
+  pch[idx2] = 16
+  cex[idx2] = 3
+
+  if (PLOT) {
+    plot(cnv_res[idx], trscr_res[idx], pch=pch, cex=cex, col=adjustcolor(cols[idx], alpha.f=0.3), 
+      main=paste(gene_name, "@", id_probe, sep=""), xlab="CN", ylab="log2(expr)")
+    # abline(h=trscr_thres_hi, v=c(cnv_thres_hi, cnv_thres_lo), col="grey", lty=2)
+    legend("bottomright", legend=paste(names(idxes), " (", sapply(idxes, length), ")", sep=""), col=c("red", "green", "blue"), pch=1)
+  }
+  return(list(idx=idxes, cols=cols))
+}
+
 #' A Function Analyses Methylome data
 #'
 #' This function analyses methylome data.
 #' @param gene A vector describing the gene (line of a bed file).
-#' @param sunexp_data A matrix of beta values.
-#' @param sunexp_platform A data frame describing CpG positions.
+#' @param meth_data A matrix of beta values.
+#' @param meth_platform A data frame describing CpG positions.
 #' @param cols A color vectors indexed by by samples names.
 #' @param PLOT A boolean defining if graphical output must be dispayed on the graphical output.
+#' @param JUST_PROBE_POS A boolean defining if function returns only probes positions.
 #' @param up_str   An integer specifying up stream size (in bp).
 #' @param dwn_str  An integer specifying down stream size (in bp).
 #' @param win_size An integer specifying slidding window size (in bp).
@@ -22,11 +176,18 @@
 #' names(cols) = rownames(sunexp_design)
 #' gene = genes[1,]
 #' res1 = analyse_meth(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE)
-#' legend("topright", col=as.numeric(unique(sunexp_design$sex)) * 2, legend=unique(sunexp_design$sex), lty=1)
+#' legend("topright", col=as.numeric(unique(sunexp_design$sex)) * 2, 
+#'        legend=unique(sunexp_design$sex), lty=1)
 #' @export
-analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_str=5000, dwn_str=5000, win_size=1500, wig_size=50) {
+analyse_meth = function(gene, meth_data, meth_platform, cols, PLOT=TRUE, up_str=5000, dwn_str=5000, win_size=1500, wig_size=50, JUST_PROBE_POS=FALSE) {
   # print(gene)
-  sample_names = colnames(sunexp_data)
+  sample_names = colnames(meth_data)
+  if (missing(cols)) {
+    cols=c()
+    cols[sample_names] = 1
+  } else {
+    cols = cols[sample_names]
+  }
   # get gene properties
   chr =            gene[[1]]
   strand =         gene[[6]]
@@ -40,24 +201,27 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
     off_set_beg = dwn_str
     off_set_end = up_str
     tss = end
-    reg_probes_names = rownames(sunexp_platform)[
-      !is.na(sunexp_platform$MAPINFO) & !is.na(sunexp_platform$CHR) &
-      sunexp_platform$CHR == chr &
-      sunexp_platform$MAPINFO > end-dwn_str &
-      sunexp_platform$MAPINFO <= end+up_str
+    reg_probes_names = rownames(meth_platform)[
+      !is.na(meth_platform$MAPINFO) & !is.na(meth_platform$CHR) &
+      meth_platform$CHR == chr &
+      meth_platform$MAPINFO > end-dwn_str &
+      meth_platform$MAPINFO <= end+up_str
     ]
   } else {
     off_set_beg = up_str
     off_set_end = dwn_str
     tss = beg
-    reg_probes_names = rownames(sunexp_platform)[
-      !is.na(sunexp_platform$MAPINFO) & !is.na(sunexp_platform$CHR) &
-      sunexp_platform$CHR == chr &
-      sunexp_platform$MAPINFO >= beg-up_str &
-      sunexp_platform$MAPINFO < beg+dwn_str
+    reg_probes_names = rownames(meth_platform)[
+      !is.na(meth_platform$MAPINFO) & !is.na(meth_platform$CHR) &
+      meth_platform$CHR == chr &
+      meth_platform$MAPINFO >= beg-up_str &
+      meth_platform$MAPINFO < beg+dwn_str
     ]
   }
-  reg_probes_pos = sunexp_platform[reg_probes_names, "MAPINFO"]
+  if (JUST_PROBE_POS) {
+    return(reg_probes_names)
+  }
+  reg_probes_pos = meth_platform[reg_probes_names, "MAPINFO"]
   tss_shift = tss %% wig_size
 
   if (length(reg_probes_pos) == 0) {
@@ -82,7 +246,7 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
     tmp_ends_idx = tmp_reg$ends_idx
     tmp_probe_idx = tmp_starts_idx:tmp_ends_idx
     tmp_probe_pos = reg_probes_pos[tmp_probe_idx]
-    tmp_d = sunexp_data[reg_probes_names[tmp_probe_idx],]
+    tmp_d = meth_data[reg_probes_names[tmp_probe_idx],]
     if (length(tmp_probe_idx) > 1) {
       tmp_d = t(tmp_d)
     } else {
@@ -141,7 +305,7 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
     # base
     plot(0, 0, col=0, xlim=c(tss-up_str, tss+up_str), ylim=c(-0.5,1), 
                       ylab="beta", 
-                      xlab=paste("chr", unique(sunexp_platform[reg_probes_names, ]$CHR), sep=""), 
+                      xlab=paste("chr", unique(meth_platform[reg_probes_names, ]$CHR), sep=""), 
                       main=gene_name)
     # gene
     polygon(c(beg,end,end,beg), c(y_base-0.01, y_base-0.01, y_base+0.01, y_base+0.01), col=1)
@@ -156,8 +320,8 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
     }  
 
     # CpG Island
-    sub_sunexp_platform = sunexp_platform[reg_probes_names,]
-    cpg_islands = get_cpg_islands(sub_sunexp_platform)
+    sub_meth_platform = meth_platform[reg_probes_names,]
+    cpg_islands = get_cpg_islands(sub_meth_platform)
     # print(paste("cpg_islands:", cpg_islands))
     pos_cpg_islands = cpg_islands$cen[chr == cpg_islands$chrom]
     # points(pos_cpg_islands, rep(y_base -0.4, length(pos_cpg_islands)), pch=16, col=adjustcolor(2, alpha.f=0.3))
@@ -165,10 +329,10 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
       polygon(c(cpg_island[["beg"]],cpg_island[["end"]],cpg_island[["end"]],cpg_island[["beg"]]), c(y_base-0.41, y_base-0.41, y_base-0.39, y_base-0.39), col=2)
     })
     # CpG probes
-    points(sunexp_platform[reg_probes_names, "MAPINFO"], rep(y_base - 0.2, length(reg_probes_names)), pch=16, col=adjustcolor(4, alpha.f=0.3))
+    points(meth_platform[reg_probes_names, "MAPINFO"], rep(y_base - 0.2, length(reg_probes_names)), pch=16, col=adjustcolor(4, alpha.f=0.3))
     # raw probes signal
-    pos_x = sunexp_platform[reg_probes_names,]$MAPINFO
-    meth_data = sunexp_data[reg_probes_names, ]
+    pos_x = meth_platform[reg_probes_names,]$MAPINFO
+    meth_data = meth_data[reg_probes_names, ]
     if (length(reg_probes_names) == 1) {
       meth_data = t(meth_data)
     }
@@ -176,7 +340,8 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
       points(pos_x, c, col=adjustcolor("grey",0.3))     
     })
     # convolved signal
-    matplot(wig_coords, wig_data_full, type="l", lty=1, col=cols[sample_names], add=TRUE)
+    matplot(wig_coords, wig_data_full, type="l", lty=1, col=adjustcolor(cols[sample_names], alpha.f=0.5), lwd=3, add=TRUE)
+    legend("bottomright", legend=paste("(", table(cols), ")", sep=""), col=names(table(cols)), lty=1)
   }
     
   if (strand == "-") {
@@ -186,8 +351,8 @@ analyse_meth = function(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE, up_
   return(wig_data_full)
 }
 
-get_cpg_islands = function(sunexp_platform) {
-  cpg_islands = sort(unique(as.character(sunexp_platform$UCSC_CpG_Islands_Name)))
+get_cpg_islands = function(meth_platform) {
+  cpg_islands = sort(unique(as.character(meth_platform$UCSC_CpG_Islands_Name)))
   cpg_island = cpg_islands[1]
   foo = apply(t(cpg_islands), 2, function(cpg_island) {
     # print(cpg_island)
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # methmybeachup
-An R package that easily convolves methylome signal.
+The __methmybeachup__ package provides a set of functions allowing to easily convolves and vizualise methylome signal across genes and CpG islands
+
+
+## Data
+
+Data set is extracted from GSE51954 Vandiver AR, Irizarry RA, Hansen KD, Garza LA et al. Age and sun exposure-related widespread genomic blocks of hypomethylation in nonmalignant skin. Genome Biol 2015 Apr 16;16:80. PMID: 25886480
+
+
+## Installation
+
+To get the current development version from github, you need first to install __devtools__. 
+Then, install ``methmybeachup``:
+
+```
+install.packages("devtools")
+devtools::install_github("fchuffar/methmybeachup")
+```
+
+
+## Example
+
+```{r}
+layout(1)
+library(methmybeachup)
+
+cols = as.numeric(sunexp_design$sex) * 2
+names(cols) = rownames(sunexp_design)
+gene = genes[6,]
+res1 = analyse_meth(gene, sunexp_data, sunexp_platform, cols, PLOT=TRUE)
+legend("topright", col=as.numeric(unique(sunexp_design$sex)) * 2, legend=unique(sunexp_design$sex), lty=1)
+
+layout(matrix(1:9, 3), respect=TRUE)
+foo = apply(genes, 1, analyse_meth, sunexp_data, sunexp_platform, cols, PLOT=TRUE)
+
+```
+
